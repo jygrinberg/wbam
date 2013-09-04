c90a7e55065a6ee3f46cc4c3048a0eb2f114e8f7
Joshua Grinberg <jgrinber@stanford.edu>
2013-09-04 00:32:59 -0400

    cleaning up commented code

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -3,43 +3,6 @@ class DataController < ApplicationController
     def index
         @metrics = Metric.all
         @metrics_distinct = Metric.all.distinct.pluck(:metric_name, :data_type, :namespace).map { |m| {metric_name: m[0], data_type: m[1], namespace: m[2] } }
-        #@health_stats = get_current_health
-    end
-
-    def get_current_health
-        period = 300
-
-        health_stats = {}
-
-        LoadBalancer.where('name != "N/A"').each_with_index do |loadBalancer, i|
-
-            service_stats = []
-
-            @metrics_distinct.each do |m|
-                metric = Metric.where(metric_name: m[:metric_name], data_type: m[:data_type], target: 'load_balancer').first
-                dp = Datapoint.where(loadBalancer_id: loadBalancer, period: period, metric_id: metric).order('timestamp').last
-
-                next if dp.nil?
-
-                count = Datapoint.where(loadBalancer_id: loadBalancer, period: period, metric_id: metric).count
-                percentile = (Datapoint.where(loadBalancer_id: loadBalancer, period: period, metric_id: metric).where('value < ?', dp.value).count.to_f) / count * 100
-
-                metric_stats = {}
-
-                metric_stats[:metric] = metric
-                metric_stats[:dp] = dp
-                metric_stats[:percentile] = percentile
-                metric_stats[:status] = percentile > 90? 1 : 0
-
-                service_stats << metric_stats
-            end
-
-            service_stats.sort_by! { |m| -m[:status] }
-
-            health_stats[loadBalancer.title.to_sym] = service_stats
-        end
-
-        return (health_stats.nil?)? Hash.new : health_stats
     end
 
     def launch_data_fetcher
@@ -55,18 +18,6 @@ class DataController < ApplicationController
         bf.delay.fetch_running_beanstalks
 
         render text: ''
-        return false
-    end
-
-    def repair_database
-        # render 'index'
-        df = DataFetcher.create
-        df.fetch_cloudwatch_data
-
-        #bf = BeanstalkFetcher.create(repeat_frequency_sec: 0)
-        #bf.fetch_running_beanstalks
-
-        render text: 'DB repair not yet implemented'
         return false
     end
 
